e8dfbe28c4ce35fdc49beccc39e84082090cdf40
Joaquin <joaquintalice@gmail.com>
2023-12-20 21:27:32 -0300

    feat: DataSeed

--- API/Program.cs
+++ API/Program.cs
@@ -39,11 +39,11 @@ var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
 }
 catch (Exception ex)
 {
     logger.LogError(ex, "An error occured during migration");
-    throw;
 }
 
 app.Run();
