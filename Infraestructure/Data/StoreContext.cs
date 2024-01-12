9c1af13589083ab1d070d49649e366d40591658b
Joaquin <joaquintalice@gmail.com>
2024-01-12 18:21:43 -0300

    fix: specifying which DbContext is being used

--- Infraestructure/Data/StoreContext.cs
+++ Infraestructure/Data/StoreContext.cs
@@ -6,7 +6,7 @@ namespace Infraestructure.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext(DbContextOptions options) : base(options)
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
         }
 
