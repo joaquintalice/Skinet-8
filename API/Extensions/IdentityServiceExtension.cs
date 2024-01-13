1a02f7f3c7d66997147d6774b8cfab02ffabf16b
Joaquin <joaquintalice@gmail.com>
2024-01-13 13:22:10 -0300

    feat: identity config & first migration

---
+++ API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,18 @@
+using Infraestructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtension
+    {
+
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppIdentityDbContext>(opt => 
+            {
+                opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+        return services;
+        }
+    }
+}
